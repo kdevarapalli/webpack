511fbda32faad21aa33bca17dfe3a35b3d40f856
Kiran Kumar Devarapalli <d.kirankumar@gmail.com>
2017-05-05 10:26:06 +1000

    Get App Version

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo exec('unzip -z "/opt/elasticbeanstalk/deploy/appsource/source_bundle" | tail -n1');
+$str = shell_exec("aws elasticbeanstalk describe-environments --environment-names Sample-env");
+
+$json = json_decode($str, true);
+
+echo $json['Environments'][0]['VersionLabel'];
+
+//echo exec('unzip -z "/opt/elasticbeanstalk/deploy/appsource/source_bundle" | tail -n1');
